8975a0fb97a3892726674509a060d75d7f38ea1b
klrotich417@gmail.com <klrotich417@gmail.com>
2024-11-14 22:11:39 +0300

    feat: initial commit

---
+++ ParamsReader.Console/Program.cs
@@ -0,0 +1,44 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using ParamsReader;
+using System.Text;
+
+BenchmarkRunner.Run<Compilation>();
+
+[MemoryDiagnoser]
+public class Compilation
+{
+
+    [Params("subUserName=18700000000", 
+        "subUserName=18700000000&tradeNo=167769&transferNo=BR915RFD&transferStatus=0&transferType=0",
+        "amount=50&bankName=工商银行&bankSubbranchName=工商银行深圳支行&charge=20&chargeMode=1&currencyID=CNY&payeeBankcard=6217003380000522687&payeeIdNo=110101200303073279&payeeName=陈珍珍&payeeTel=13163701234&subUserName=18700000000&tradeNo=167769&transferNo=BR915RFD&transferStatus=1&transferType=0")]
+    public string Data { get; set; }
+
+
+    [Benchmark]
+    public string ParseAdvance()
+    {
+        var reader = new Reader(Data);
+
+        return reader.Parse();
+    }
+
+    [Benchmark]
+    public string ParseSimpleNoSpan()
+    {
+        var builder = new StringBuilder();
+
+        var dataArray = Data.Split('&');
+
+        for (int i = 0; i < dataArray.Length; i++)
+        {
+            var value = dataArray[i].Split('=');
+
+            builder.Append(value[0])
+                .Append(':')
+                .Append(value[1]);
+        }
+
+        return builder.ToString();
+    }
+}
---
+++ ParamsReader/Reader.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ParamsReader
+{
+    public class Reader 
+    {
+        private int _position;
+        private readonly string _text;
+        private int _start;
+        private int _parsePostion;
+
+        private string _value;
+
+        private TokenTypes _tokenType;
+
+        private readonly List<DataType> _tokens = [];
+
+        private static StringBuilder _builder = new();
+
+        public Reader(string text)
+        {
+            _start = _position;
+            _text = text;
+        }
+
+        public char Current => Peek(0);
+
+        public DataType ParseCurrent => ParsePeek(0);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public DataType ParsePeek(int offset)
+        {
+            var pos = _parsePostion + offset;
+
+            if ((uint)pos >= (uint)_tokens.Count)
+            {
+                return _tokens[_tokens.Count + 1];
+            }
+
+            return _tokens[pos];
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public DataType Advance()
+        {
+            var current = ParseCurrent;
+            _parsePostion++;
+
+            return current;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public DataType Match(TokenTypes type)
+        {
+            if(ParseCurrent.type == type)
+            {
+                return Advance();
+            }
+            throw new InvalidDataException("Unexpected value");
+        }
+
+        public char Peek(int offset)
+        {
+            var pos = _position + offset;
+
+            if ((uint)pos < (uint)_text.Length)
+            {
+                return _text[pos];
+            }
+
+            return '\0';
+        }
+
+        public DataType Identify()
+        {
+            switch (Current)
+            {
+                case '\0':
+                    _tokenType = TokenTypes.Eos;
+                    break;
+                case '&':
+                    _position++;
+                    _value = null;
+                    _tokenType = TokenTypes.Ampersand;
+                    break;
+                case '=':
+                    _position++;
+                    if (char.IsLetterOrDigit(Current))
+                    {
+                        while (char.IsLetterOrDigit(Current))
+                        {
+                            _position++;
+                        }
+
+                        var length = _position - _start;
+
+                        var text = _text.Substring(_start, length);
+
+                        _value = text;
+                        _tokenType = TokenTypes.Value;
+                        _start = _position + 1;
+                    }
+               
+                    break;
+                default:
+                    if (char.IsLetter(Current))
+                    {
+                        while (char.IsLetter(Current))
+                        {
+                            _position++;
+                        }
+
+                        var length = _position - _start;
+
+                        var text = _text.Substring(_start, length);
+
+                        _value = text;
+                        _tokenType = TokenTypes.Key;
+
+                        //we set the new start position
+                        _start = _position + 1;
+                    }
+                    break;
+            }
+            return new DataType(_tokenType, _value);
+        }
+
+        public string Parse()
+        {
+            DataType token;
+
+            _builder.Clear();
+
+            //Read
+            do
+            {
+                token = Identify();
+
+                if (token.type != TokenTypes.Ampersand)
+                {
+                    _tokens.Add(token);
+                }
+
+            } while (token.type != TokenTypes.Eos);
+
+
+            while(ParseCurrent.type != TokenTypes.Eos)
+            {
+                //parse 'key=value'
+                //match key
+                if(ParseCurrent.type == TokenTypes.Key)
+                {
+                    var key = Match(TokenTypes.Key);
+                    _builder.Append(key.data);
+                }
+
+                _builder.Append(':');
+
+
+                //match value
+                if (ParseCurrent.type == TokenTypes.Value)
+                {
+                    var value = Match(TokenTypes.Value);
+                    _builder.Append(value.data);
+                }
+            }
+
+            return _builder.ToString();
+        }
+
+    }
+
+
+    public record struct DataType(TokenTypes type, string data);
+
+    public enum TokenTypes
+    {
+        Eos = 0,
+        Key = 1, 
+        Value = 2,
+        Ampersand = 3,
+        Equals = 4
+    }
+}
